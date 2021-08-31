1a42c538c3842a80fda9fcec9a1577facdfdee81
webtraveler-br <mlucasprestes@gmail.com>
2021-08-31 11:53:01 -0300

    [add] FS async

---
+++ 7-fs-async.js
@@ -0,0 +1,28 @@
+const { readFile, writeFile } = require("fs");
+
+// lots of callbacks but seems better
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile("./content/second.txt", "utf8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile(
+      "./content/result2.txt",
+      `There it is! your results sr:\n- ${first} \n- ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result);
+      }
+    );
+  });
+});
